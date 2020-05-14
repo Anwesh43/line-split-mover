0a5543643b8c40243f01401b10cc26c340652ef7
Anwesh43 <anweshthecool0@gmail.com>
2020-05-14 12:19:42 +0530

    adding LineSplitMoverContainer component

---
+++ src/LineSplitMoverContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {useAnimatedScale, useDimension} from './hooks'
+import LineSplitMoverPresentational from './LineSplitMoverPresentational'
+
+const LineSplitMoverContainer = (props) => {
+    const {scale, start} = useAnimatedScale(0.02, 20)
+    const {w, h} = useDimension()
+    return <LineSplitMoverPresentational w = {w} h = {h} scale = {scale} onClick = {start}>
+          </LineSplitMoverPresentational>
+}
+
+export default LineSplitMoverContainer
